ad1a87c929c7ac9be329fc3ebe3d2b16dc0b50c5
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-12-05 18:53:12 +0200

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,4 @@
-import Container from './Container';
-import Row from './Row';
-import Column from './Column';
-import Hidden from './Hidden';
-
-export {
-  Container,
-  Row,
-  Column,
-  Hidden
-};
+export Container from './Container';
+export Row from './Row';
+export Column from './Column';
+export Hidden from './Hidden';
