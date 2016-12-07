092243e2d49d135b0065c19c30d25a5bbaaa1707
AndreyShpilevoy <andrey.shpilevoy@gmail.com>
2016-12-07 15:45:29 +0200

    added fake tests updated package version

---
+++ __tests__/empty.js
@@ -0,0 +1,10 @@
+/*eslint no-undef: 'off'*/
+/* eslint import/no-extraneous-dependencies: 'off' */
+
+// import React from 'react';
+// import {shallow} from 'enzyme';
+
+describe('Empty test case', () => {
+  it('enpty test',() => {
+  });
+});
